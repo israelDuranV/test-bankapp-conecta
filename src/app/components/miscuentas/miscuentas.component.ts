2a08e3c10a2ebea514d3702f3ff3339c74e386cd
Israel Duran <israel_duran@outlook.com>
2019-09-06 14:36:41 -0500

    Funcionalidad JSON
    Se a agregado el alert al dar aceptar en el formulario de crear tarjeta

--- src/app/components/miscuentas/miscuentas.component.ts
+++ src/app/components/miscuentas/miscuentas.component.ts
@@ -11,6 +11,8 @@ export class MiscuentasComponent implements OnInit {
   saldos:any = {};
   tarjetas:any = {};
   movimientos:any = {};
+  mijson:any = {};
+  visible:string = "novisible";
   constructor( private http: HttpClient) { 
     this.http.get('https://api.sheety.co/fed17b86-497b-4a6e-9388-f38a495dbd43')
     .subscribe( (response: any)=>{
@@ -24,6 +26,22 @@ export class MiscuentasComponent implements OnInit {
     .subscribe((response: any)=>{
       this.movimientos = response;
     })
+  }
+  public sendForm(numeroTarjeta, cuenta,issuer,nombreTarjeta,marca,estatus,saldo,tipoCuenta){
+    this.mijson = [{
+                  "numero_tarjeta": numeroTarjeta, 
+                  "cuenta": cuenta,
+                  "issuer": issuer,
+                  "nombre_tarjeta": nombreTarjeta,
+                  "marca": marca,
+                  "estatus": estatus,
+                  "saldo":saldo,
+                  "tipo_cuenta":tipoCuenta
+                }];
+    this.visible = "visible";
+  }
+  public cancelarForm(){
+    this.visible = "novisible";
   }
 
   ngOnInit() {
