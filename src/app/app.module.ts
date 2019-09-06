1616be5bd1ed94be82d9d2adafbdc82012f0f931
Israel Duran <israel_duran@outlook.com>
2019-09-05 21:42:12 -0500

    Estructura Bankapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_ROUTING } from './app.routes';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
+    APP_ROUTING,
     BrowserModule,
     AppRoutingModule
   ],
